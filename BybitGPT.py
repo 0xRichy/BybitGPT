b1eeac3e6aa749a09b55f70af32c764ee4a8ede3
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-07-26 16:55:44 +0200

    Create BybitGPT.py

---
+++ BybitGPT.py
@@ -0,0 +1,99 @@
+import ccxt
+import openai
+import logging
+import requests
+import time
+import telegram
+
+# Vous devez créer vos propres clés API sur Bybit et les remplacer ici
+apiKey = 'your_api_key'
+secret = 'your_secret_key'
+
+openai.api_key = 'your_openai_api_key'
+
+exchange = ccxt.bybit({
+    'apiKey': apiKey,
+    'secret': secret,
+    'enableRateLimit': True,
+    'options': {
+        'defaultType': 'future'
+    }
+})
+
+# Set up logging
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+def set_leverage(leverage: int):
+    # La fonction set_leverage reste inchangée
+
+def get_balance():
+    # La fonction get_balance reste inchangée
+
+def get_price():
+    # La fonction get_price reste inchangée
+
+def place_order(side, amount):
+    # La fonction place_order reste inchangée
+
+def get_openai_decision(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=50
+    )
+    
+    # Extraction de la réponse de ChatGPT
+    decision = response.choices[0].text.strip()
+
+    return decision
+
+# Nouvelle fonction pour l'interaction automatique avec ChatGPT et envoi de notifications Telegram
+def automatic_chat_with_chatgpt():
+    print("ChatGPT: Hello! I'm here to assist you with trading decisions.")
+    
+    # Initialise la connexion avec Telegram (remplacez 'your_bot_token' par le token de votre bot Telegram)
+    bot_token = 'your_bot_token'
+    bot = telegram.Bot(token=bot_token)
+    
+    # Remplacez 'your_chat_id' par l'ID du chat sur lequel vous souhaitez recevoir les notifications
+    chat_id = 'your_chat_id'
+
+    while True:
+        # Demande à ChatGPT quelle action prendre en fonction des conditions actuelles du marché
+        market_conditions_prompt = "Given the current market conditions, should we buy or sell BTC?"
+        decision = get_openai_decision(market_conditions_prompt)
+
+        if decision == 'buy':
+            balance = get_balance()
+            if balance >= 10:
+                set_leverage(25)  # Set leverage to 25x
+                place_order('BUY', 10)
+                message = "Bot: Placed a BUY order for 10 BTC."
+                print(message)
+                bot.send_message(chat_id=chat_id, text=message)
+            else:
+                message = "Bot: Not enough USDT balance to execute a BUY order."
+                print(message)
+                bot.send_message(chat_id=chat_id, text=message)
+        elif decision == 'sell':
+            btc_balance = exchange.fetch_balance()['BTC']
+            if btc_balance['free'] > 0:
+                set_leverage(25)  # Set leverage to 25x
+                place_order('SELL', btc_balance['free'])
+                message = f"Bot: Placed a SELL order for {btc_balance['free']} BTC."
+                print(message)
+                bot.send_message(chat_id=chat_id, text=message)
+            else:
+                message = "Bot: No BTC balance to execute a SELL order."
+                print(message)
+                bot.send_message(chat_id=chat_id, text=message)
+        
+        # Attendre quelques secondes avant de demander une autre action à ChatGPT
+        time.sleep(10)
+
+# Mettez en commentaire cette ligne pour éviter l'interaction manuelle
+#interact_with_chatgpt()
+
+# Lancement de l'interaction automatique avec ChatGPT et envoi de notifications Telegram
+automatic_chat_with_chatgpt()
